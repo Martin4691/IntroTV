965b2b7f4abe83a42819a4c707707ea62df6a8bb
Martín4691 <martinsm1991@gmail.com>
2020-12-02 16:17:34 +0100

    Ahora estamos con la xib de la Cell. AF installed.

---
+++ Table View Navigation/IntroTV/Controllers/CellVC.swift
@@ -0,0 +1,24 @@
+//
+//  CellVC.swift
+//  IntroTV
+//
+//  Created by Martín on 02/12/2020.
+//
+
+import Foundation
+import UIKit
+
+class CellVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Table View Navigation/IntroTV/Models/Endpoints.swift
@@ -0,0 +1,20 @@
+//
+//  Endpoints.swift
+//  IntroTV
+//
+//  Created by Martín on 02/12/2020.
+//
+
+import Foundation
+import UIKit
+
+
+struct Endpoints {
+    static let movieDatabase: String = "https://developers.themoviedb.org/3/getting-started/introduction"
+    
+    static let moviePopular: String = "https://developers.themoviedb.org/3/discover/movie-discover"
+    
+    static let movieInfo: String = "https://developers.themoviedb.org/3/movies/get-movie-details"
+    
+    
+}
